c18991c3e20a880ce868c2b06dd6a4dde8d36b8d
unknown <rosember.12@gmail.com>
2023-03-13 10:04:09 -0500

    Added special parse test

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# java-test
+# Java Exercises
+
+## Overview
+This is a set of java exercises done by Me @Rosemberg.porras
+
+
+## Exercises
+
+1. SpecialParse.java:
+```text
+Write a program that parses a sentence and replaces each word with the following: 
+1) The first letter of the word
+2) The number of distinct characters between first and last character
+3) The last letter of the word. 
+For example, Smooth would become S3h. 
+Words are separated by spaces or non-alphabetic characters and these separators should be maintained in their original form and location in the answer. 
+A few of the things we will be looking at is accuracy, efficiency, solution completeness. 
+```
+
+## License
+MIT
+
+The code in this repository is covered by the included license.
---
+++ src/main/java/org/rosemberg/test/SpecialParse.java
@@ -0,0 +1,57 @@
+package org.rosemberg.test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SpecialParse {
+
+    public static String wordParser(String input) {
+
+        final char[] letters= input.toCharArray();
+
+        final List<String> words= new ArrayList<>();
+
+        int i=0;
+
+        while(i<letters.length){
+
+            final StringBuilder sb= new StringBuilder();
+            sb.append(letters[i]);
+
+            final Set<Character> intermedialLetters=new HashSet<>();
+
+            i++;
+
+            while(i<letters.length
+                    && Character.isAlphabetic(letters[i])
+                    && i+1<letters.length
+                    && Character.isAlphabetic(letters[i+1]))
+                intermedialLetters.add(letters[i++]);
+
+
+            if(!intermedialLetters.isEmpty()){
+                sb.append(intermedialLetters.size());
+            }
+
+            sb.append(letters[i++]);
+
+            for(;i<letters.length && letters[i]!=' ';i++)
+                sb.append(letters[i]);
+
+            words.add(sb.toString());
+
+            i++;
+
+        }
+
+        return String.join(" ", words);
+    }
+
+    public static void main (String[] args) {
+        String output = wordParser("Creativity is thinking-up new things. Innovation is doing new things!");
+        assert "C6y is t4g-up n1w t4s. I6n is d3g n1w t4s!".equals(output);
+        System.out.println(output);
+    }
+}
